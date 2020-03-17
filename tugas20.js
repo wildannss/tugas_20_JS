4b0acd112e56e0de35648a27fbd5269d3891f997
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:27:17 +0700

    tugas niomic

---
+++ tugas20.js
@@ -0,0 +1,7 @@
+function tugas20(){
+    let x = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+    console.log(x.match("are"));
+    console.log(x.match("word"));
+};
+
+tugas20();
